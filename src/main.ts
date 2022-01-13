4c4b20b1784056b7ae949836e0bbd85dcaf31ebe
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-14 02:24:45 +0700

    chore: explain validation setting

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
+      whitelist: true, // filter out fields not declared in dto
     }),
   );
 
