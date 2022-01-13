64c6e76fa99ce0d68ba399120fc5b682af07b171
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-14 02:33:22 +0700

    feat(user): impl service and use from ctrl

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepo: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.usersRepo.create({ email, password });
+
+    return this.usersRepo.save(user);
+  }
+}
