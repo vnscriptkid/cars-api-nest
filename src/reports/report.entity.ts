86ece6360372482cc17e5b500b05d11d0616bcbd
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-14 01:45:38 +0700

    feat(report): add price to Report

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,7 +1,10 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  price: number;
 }
