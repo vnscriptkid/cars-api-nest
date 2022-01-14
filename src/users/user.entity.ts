c07e504a2f47db363af8068550fffc8cf7c60344
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-14 18:52:12 +0700

    feat(users): filter out password using interceptor

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import {
   AfterInsert,
   AfterRemove,
@@ -16,6 +17,7 @@ export class User {
   email: string;
 
   @Column()
+  @Exclude() // exclude while serializing into object
   password: string;
 
   @AfterInsert()
