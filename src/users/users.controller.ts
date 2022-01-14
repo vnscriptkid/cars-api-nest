b2a391b1b56fa469a044c35a8baa9f7ee0ea5c66
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-15 00:39:02 +0700

    refactor(users): controller-wide interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,6 +17,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -28,7 +29,6 @@ export class UsersController {
 
   // @UseInterceptors(ClassSerializerInterceptor)
   // @UseInterceptors(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
   @Get(':id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(+id);
