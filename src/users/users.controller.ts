77d469fe9e0743e8ec7c75473c91a66e4befccc7
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-14 18:43:58 +0700

    feat(users): 404 if user not found

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -22,8 +23,12 @@ export class UsersController {
   }
 
   @Get(':id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(+id);
+
+    if (!user) throw new NotFoundException('User not found.');
+
+    return user;
   }
 
   @Get()
