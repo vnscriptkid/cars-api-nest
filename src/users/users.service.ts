21c07216d59689c46ac86fd0382a074aa72d78a6
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-14 18:11:57 +0700

    feat(users): service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -13,5 +13,31 @@ export class UsersService {
     const user = this.usersRepo.create({ email, password });
 
     return this.usersRepo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.usersRepo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.usersRepo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) throw new NotFoundException('User not found.');
+
+    Object.assign(user, attrs);
+
+    return this.usersRepo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) throw new NotFoundException('User not found.');
+
+    return this.usersRepo.remove(user);
   }
 }
