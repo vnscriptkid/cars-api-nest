109bb42e7cb11afc31312a816af7ec65a64d0ebb
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-16 00:15:43 +0700

    refactor(current-user): globally scoped interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,18 +11,15 @@ import {
   Delete,
   NotFoundException,
   Session,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
 
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 @Controller('auth')
 export class UsersController {
   constructor(
