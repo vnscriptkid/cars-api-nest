7282689ba97a2d560fb955f9e60a2cbe989ea2f2
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-16 19:21:01 +0700

    test(user.controller): createUser works

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-describe.skip('UsersController', () => {
+describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
+    const fakeAuthService = {
+      signup: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
+
+    const fakeUsersService = {};
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +34,22 @@ describe.skip('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('createUser should returns correct user', async () => {
+    const session: any = {};
+
+    const user = await controller.createUser(
+      { email: 'someone@gmail.com', password: '123456' } as User,
+      session,
+    );
+
+    expect(user).toMatchObject({
+      id: 1,
+      email: 'someone@gmail.com',
+      password: '123456',
+    });
+
+    expect(session.userId).toBe(1);
   });
 });
