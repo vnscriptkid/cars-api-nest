68fb31490df274f320a1b664bcaede94cc7488b3
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-17 19:23:08 +0700

    feat(report): attach current user when creating

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/CreateReportDto';
 import { Report } from './report.entity';
@@ -10,8 +11,10 @@ export class ReportsService {
     @InjectRepository(Report) private readonly reportsRepo: Repository<Report>,
   ) {}
 
-  async create(createReportDto: CreateReportDto) {
+  async create(createReportDto: CreateReportDto, user: User) {
     const report = this.reportsRepo.create(createReportDto);
+
+    report.user = user;
 
     return this.reportsRepo.save(report);
   }
