30d9cbbb94e1ad2d96122b932225faecdbdce389
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-17 18:57:20 +0700

    feat(report): create new report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/CreateReportDto';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private readonly reportsRepo: Repository<Report>,
+  ) {}
+
+  async create(createReportDto: CreateReportDto) {
+    const report = this.reportsRepo.create(createReportDto);
+
+    return this.reportsRepo.save(report);
+  }
+}
