55d7b8df0d6ca695f95b6d4d0640900e874471cb
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-18 11:14:49 +0700

    chore(deployment): setup prod env

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
